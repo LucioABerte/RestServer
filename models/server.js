5d24887eb9eb23de43944ea1933d79e23b2cdf20
Lucio Berté <104603180+LucioABerte@users.noreply.github.com>
2023-10-20 19:05:05 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 class Server {
     constructor() {
         this.app = express()
-        this.port = process.env.PORT
+        this.port = process.env.PORT || 3000
         this.usersPath = '/api/users'
 
         //Middlewares
